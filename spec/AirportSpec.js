6ff20c6855e5066bf88f119d6abe35cfaf475e5c
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-11-21 16:16:21 +0000

    adds user stories 1, 2, 3 and 4

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -8,10 +8,43 @@ describe('Airport', function(){
   });
 
   describe('Airport to instruct the plane to land', function(){
-
     it('prints a massage when plane landed', function(){
-
+      spyOn(airport, 'isStormy').and.returnValue(false)
       expect(airport.land(plane)).toEqual('Plane landed')
+    });
+
+    it('updates the hangar', function(){
+      spyOn(airport, 'isStormy').and.returnValue(false)
+      airport.land(plane)
+      expect(airport._hangar.length).toEqual(1)
+    });
+
+    it('prevents landing when stormy with a message', function(){
+      spyOn(airport, 'isStormy').and.returnValue(true)
+      expect(airport.land(plane)).toEqual('Sorry, stormy weather')
+      expect(airport._hangar.length).toEqual(0)
+    });
+  });
+
+  describe('Airport to instruct the plane to take off', function(){
+    beforeEach(function(){
+      airport._hangar.push(plane);
+    });
+    it('prints a massage when plane has taken off', function(){
+      spyOn(airport, 'isStormy').and.returnValue(false)
+      expect(airport.takeOff(plane)).toEqual('Plane took off')
+    });
+
+    it('updates the hangar', function(){
+      spyOn(airport, 'isStormy').and.returnValue(false)
+      airport.takeOff(plane)
+      expect(airport._hangar.length).toEqual(0)
+    });
+
+    it('prevents take off when stormy with a message', function(){
+      spyOn(airport, 'isStormy').and.returnValue(true)
+      expect(airport.takeOff(plane)).toEqual('Sorry, stormy weather')
+      expect(airport._hangar.length).toEqual(1)
     });
   });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,10 +1,21 @@
-
-
-var  Airport = function() {
-  this._hangar = []
+function Airport(){
+  this._hangar = [];
 }
 
 Airport.prototype.land = function (plane) {
-  // this._hangar.push(plane)
-  return 'Plane landed' ;
+  if(this.isStormy()) return 'Sorry, stormy weather';
+  this._hangar.push(plane);
+  return 'Plane landed';
+};
+
+Airport.prototype.takeOff = function (plane) {
+  if(this.isStormy()) return 'Sorry, stormy weather';
+  this._hangar.pop();
+  return 'Plane took off' ;
+};
+
+Airport.prototype.isStormy = function () {
+  var number = Math.random();
+  if(number >= 0.5) return true;
+  return false;
 };
